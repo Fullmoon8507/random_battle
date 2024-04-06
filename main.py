6604f4afb8244a2d221f3e74474634759f979879
fullmoon <xxx@sample.com>
2024-04-06 12:59:41 +0900

    攻撃力の概念を追加

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 class Character:
 
     def __init__(self):
-        # 暫定でHPを100固定とする。
+        # 各変数に初期値を設定。
         self.hp = 100
+        self.offence = 20
 
 
 if __name__ == "__main__":
@@ -22,17 +23,20 @@ if __name__ == "__main__":
 
         # 攻撃
         # 順番は player1 -> player2 の順番で攻撃する。
-        # ダメージも20固定で。
-        player2.hp -= 20
-        print("player1 が player2 に 20 のダメージ。")
+        # ダメージ＝キャラクターの offenc
+        damage = player1.offence
+        player2.hp -= damage
+        print("player1 が player2 に " + str(damage) + " のダメージ。")
 
         # 攻撃後、攻撃されたキャラクターのHPが0以下だったら。バトル終了
         if player2.hp <= 0:
             break
 
         # 次の攻撃
-        player1.hp -= 20
-        print("player2 が player1 に 20 のダメージ。")
+        # ダメージ＝キャラクターの offenc
+        damage = player2.offence
+        player1.hp -= player2.offence
+        print("player2 が player1 に " + str(damage) + " のダメージ。")
 
         # 攻撃後、攻撃されたキャラクターのHPが0以下だったら。バトル終了
         if player1.hp <= 0:
