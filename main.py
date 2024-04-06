c5c50dae718820f951cb1f2cfec6de744c19d3a4
fullmoon <xxx@sample.com>
2024-04-06 16:13:45 +0900

    クラスのリファクタリング。

--- main.py
+++ main.py
@@ -11,6 +11,31 @@ class Battle:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
+    
+
+    def __doBattleOneTurn(self):
+
+        # 攻撃
+        # 順番は player1 -> player2 の順番で攻撃する。
+        # ダメージ＝キャラクターの offenc
+        damage = player1.offence
+        player2.hp -= damage
+        print("player1 が player2 に " + str(damage) + " のダメージ。")
+
+        # 攻撃後、攻撃されたキャラクターのHPが0以下だったら。バトル終了
+        if player2.hp <= 0:
+            return True
+
+        # 次の攻撃
+        # ダメージ＝キャラクターの offenc
+        damage = player2.offence
+        player1.hp -= player2.offence
+        print("player2 が player1 に " + str(damage) + " のダメージ。")
+
+        # 攻撃後、攻撃されたキャラクターのHPが0以下だったら。バトル終了
+        if player1.hp <= 0:
+            return True
+
 
     # バトル実施
     # 1 vs 1 で実施
@@ -24,30 +49,14 @@ class Battle:
             turn_count += 1
             print("ターン" + str(turn_count))
 
-            # 攻撃
-            # 順番は player1 -> player2 の順番で攻撃する。
-            # ダメージ＝キャラクターの offenc
-            damage = player1.offence
-            player2.hp -= damage
-            print("player1 が player2 に " + str(damage) + " のダメージ。")
-
-            # 攻撃後、攻撃されたキャラクターのHPが0以下だったら。バトル終了
-            if player2.hp <= 0:
+            # 1ターン分のバトルを実施
+            result = self.__doBattleOneTurn()
+            if result:
                 break
-
-            # 次の攻撃
-            # ダメージ＝キャラクターの offenc
-            damage = player2.offence
-            player1.hp -= player2.offence
-            print("player2 が player1 に " + str(damage) + " のダメージ。")
-
-            # 攻撃後、攻撃されたキャラクターのHPが0以下だったら。バトル終了
-            if player1.hp <= 0:
-                break
-
+    
             # 次のターンとし、次のフェーズを実施する。
             print("")
-    
+
         if player1.hp <= 0:
             print("player2 の勝ち。")
         else:
