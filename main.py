4ba650233acbbcd97ab8f490a4cf0a752d65c87f
fullmoon <xxx@sample.com>
2024-04-06 16:35:06 +0900

    キャラクターの名前を追加と攻撃の処理をメソッド化。

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 class Character:
 
-    def __init__(self):
+    def __init__(self, name: str):
+        # 名前は必ず設定
+        self.name = name
+
         # 各変数に初期値を設定。
         self.hp = 100
         self.offence = 20
@@ -11,16 +14,24 @@ class Battle:
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
+
+
+    # 攻撃
+    def __doAttack(self, offenceCharacter :Character, deffenceCharacter :Character):
+        
+        # ダメージ＝キャラクターの offenc
+        damage = offenceCharacter.offence
+        deffenceCharacter.hp -= damage
+        print(offenceCharacter.name + " が " + deffenceCharacter.name + " に " + str(damage) + " のダメージ。")
     
 
+    # 1ターン分のバトル
     def __doBattleOneTurn(self):
 
         # 攻撃
         # 順番は player1 -> player2 の順番で攻撃する。
         # ダメージ＝キャラクターの offenc
-        damage = player1.offence
-        player2.hp -= damage
-        print("player1 が player2 に " + str(damage) + " のダメージ。")
+        self.__doAttack(player1, player2)
 
         # 攻撃後、攻撃されたキャラクターのHPが0以下だったら。バトル終了
         if player2.hp <= 0:
@@ -28,9 +39,7 @@ class Battle:
 
         # 次の攻撃
         # ダメージ＝キャラクターの offenc
-        damage = player2.offence
-        player1.hp -= player2.offence
-        print("player2 が player1 に " + str(damage) + " のダメージ。")
+        self.__doAttack(player2, player1)
 
         # 攻撃後、攻撃されたキャラクターのHPが0以下だったら。バトル終了
         if player1.hp <= 0:
@@ -66,8 +75,8 @@ class Battle:
 if __name__ == "__main__":
 
     # キャラクター作成
-    player1 = Character()
-    player2 = Character()
+    player1 = Character("player1")
+    player2 = Character("player2")
 
     # バトル実施
     battle = Battle(player1, player2)
